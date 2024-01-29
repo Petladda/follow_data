c4a47c2759990bbf92460e5e856fa2c856f17e1a
Helio Trope <petladda.94@gmail.com>
2024-01-29 17:32:52 +0700

    fix:register api

--- projectall/serializers.py
+++ projectall/serializers.py
@@ -14,10 +14,10 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = AppUser(username=validated_data["username"])   
-        """user = AppUser(username=validated_data["id_student"])
-        user = AppUser(username=validated_data["first_name"])
-        user = AppUser(username=validated_data["last_name"])"""
-        user.set_password(validated_data["password"])
+        user = AppUser.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.role = "STD"
         user.save()
+
         return user
+        
