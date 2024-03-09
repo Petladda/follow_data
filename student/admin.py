76768f6913b66ce983942f9dce296e33763ff4df
Helio Trope <petladda.94@gmail.com>
2024-03-09 23:26:29 +0700

    fix: delete folder student

--- student/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from student.models import Student
-
-# Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ["student_id","first_name","last_name","group"]
-
-admin.site.register(Student,StudentAdmin)
